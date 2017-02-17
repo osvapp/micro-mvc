55073b34a4b2720babcc8cc8a944d3c12cfbc3b4
gregoiremielle <gregoiremielle@gmail.com>
2017-02-17 18:47:30 +0100

    ✨ Add Twig templating support

--- public/index.php
+++ public/index.php
@@ -3,17 +3,21 @@
 require('../vendor/autoload.php');
 $router = new \App\Config\Router\Router($_GET);
 
+$loader = new Twig_Loader_Filesystem(__DIR__ . '/../App/Views');
+$twig   = new Twig_Environment($loader, [
+    'cache' => false
+]);
 
 $router->get('/', function() {
-    echo "Bienvenue sur ma homepage !";
+    echo 'Home';
 });
 
 $router->get('/posts/:id/edit', function($id) {
-    echo "Editer l'article $id";
+    echo "Edit article $id";
 })->with('id', '[0-9]+');
 
 $router->error(function() {
-    echo "rien";
+    echo 'Nothing';
 });
 
 $router->run();

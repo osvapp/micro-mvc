6204727d320acfcedcbf8aa017dfe8d8cfe22ac5
gregoiremielle <gregoiremielle@gmail.com>
2017-03-13 19:03:55 +0100

    🔥 Removing unnecessary methods

--- App/Controllers/PostsController.php
+++ App/Controllers/PostsController.php
@@ -3,65 +3,20 @@ namespace App\Controllers;
 
 use \App\System\App;
 use \App\System\ImageUpload;
-use \App\System\Settings;
-use \App\Controllers\Controller;
-use \App\Models\RevisionsModel;
-use \App\Models\CategoriesModel;
-use \App\Models\ProductsModel;
-use \App\Models\ReportsModel;
+use \App\Models\PostsModel;
 use \App\System\FormValidator;
 
 class PostsController extends Controller {
 
-    public function blank() {
-        $this->render('pages/index.twig', [
-            'title'       => 'Home',
-            'description' => 'Just a simple inventory management system.'
-        ]);
-    }
-
-    public function index() {
-        $model = new ProductsModel();
-        $value = $model->value();
-        $count = $model->count();
-        $average_quantity = $model->average('quantity');
-        $average_price    = $model->average('price');
-        $lows_products    = $model->low(5);
-
-        $model2 = new CategoriesModel();
-        $categories_value = $model2->allotment();
-
-        $model3  = new ReportsModel();
-        $reports = $model3->all();
-
-        $stats = [
-            'value' => $value,
-            'count' => $count,
-            'average_quantity' => round($average_quantity),
-            'average_price'    => '$' .round($average_price, 2),
-            'lows_products'    => $lows_products,
-            'reports'          => $reports
-        ];
-
-        $this->render('pages/admin/dashboard.twig', [
-            'title'       => 'Dashboard',
-            'description' => 'Dashboard - Just a simple inventory management system.',
-            'page'        => 'dashboard',
-            'stats'       => $stats
-        ]);
-    }
-
     public function all() {
-        $model = new ProductsModel();
+        $model = new PostsModel();
         $data  = $model->all();
-        $count = count($data);
 
-        $this->render('pages/admin/products.twig', [
+        $this->render('posts.twig', [
             'title'       => 'Products',
             'description' => 'Products - Just a simple inventory management system.',
             'page'        => 'products',
-            'products'    => $data,
-            'count'       => $count
+            'products'    => $data
         ]);
     }
 
@@ -77,7 +32,6 @@ class PostsController extends Controller {
             $validator = new FormValidator();
             $validator->notEmpty('title', $title, "Your title must not be empty");
             $validator->notEmpty('description', $description, "Your description must not be empty");
-            $validator->validCategory('category', $category, "Your category must be valid");
             $validator->isNumeric('price', $price, "Your price must be a number");
             $validator->isInteger('quantity', $quantity, "Your quantity must be a number");
             $validator->validImage('media', $media, "You didn't provided a media or it is invalid");
@@ -86,7 +40,7 @@ class PostsController extends Controller {
                 $upload    = new ImageUpload();
                 $media_url = $upload->add($media);
 
-                $model = new ProductsModel();
+                $model = new PostsModel();
                 $model->create([
                     'title'       => $title,
                     'description' => $description,
@@ -100,14 +54,11 @@ class PostsController extends Controller {
             }
 
             else {
-                $model = new CategoriesModel();
-                $categories  = $model->all();
                 $this->render('pages/admin/products_add.twig', [
                     'title'       => 'Add product',
                     'description' => 'Products - Just a simple inventory management system.',
                     'page'        => 'products',
                     'errors'      => $validator->getErrors(),
-                    'categories'  => $categories,
                     'data'        => [
                         'title'       => $title,
                         'description' => $description,
@@ -119,13 +70,10 @@ class PostsController extends Controller {
         }
 
         else {
-            $model = new CategoriesModel();
-            $categories  = $model->all();
             $this->render('pages/admin/products_add.twig', [
                 'title'       => 'Add product',
                 'description' => 'Products - Just a simple inventory management system.',
-                'page'        => 'products',
-                'categories'  => $categories
+                'page'        => 'products'
             ]);
         }
     }
@@ -141,12 +89,11 @@ class PostsController extends Controller {
             $validator = new FormValidator();
             $validator->notEmpty('title', $title, "Your title must not be empty");
             $validator->notEmpty('description', $description, "Your description must not be empty");
-            $validator->validCategory('category', $category, "Your category must be valid");
             $validator->isNumeric('price', $price, "Your price must be a number");
             $validator->isInteger('quantity', $quantity, "Your quantity must be a number");
 
             if($validator->isValid()) {
-                $model = new ProductsModel();
+                $model = new PostsModel();
                 $model->update($id, [
                     'title'       => $title,
                     'description' => $description,
@@ -155,30 +102,15 @@ class PostsController extends Controller {
                     'quantity'    => $quantity
                 ]);
 
-                $revisions = new RevisionsModel();
-                $revisions->create([
-                    'type'    => 'products',
-                    'type_id' => $id,
-                    'user'    => $_SESSION['auth']
-                ]);
-
                 App::redirect('admin/products');
             }
 
             else {
-                $model = new CategoriesModel();
-                $categories  = $model->all();
-
-                $model2 = new RevisionsModel();
-                $revisions = $model2->revisions($id, 'products');
-
                 $this->render('pages/admin/products_add.twig', [
                     'title'       => 'Edit product',
                     'description' => 'Products - Just a simple inventory management system.',
                     'page'        => 'products',
                     'errors'      => $validator->getErrors(),
-                    'revisions'   => $revisions,
-                    'categories'  => $categories,
                     'data'        => [
                         'title'       => $title,
                         'description' => $description,
@@ -191,29 +123,21 @@ class PostsController extends Controller {
         }
 
         else {
-            $model = new CategoriesModel();
-            $categories  = $model->all();
-
-            $model2 = new ProductsModel();
+            $model2 = new PostsModel();
             $data   = $model2->find($id);
-
-            $model3 = new RevisionsModel();
-            $revisions = $model3->revisions($id, 'products');
 
             $this->render('pages/admin/products_edit.twig', [
                 'title'       => 'Edit product',
                 'description' => 'Products - Just a simple inventory management system.',
                 'page'        => 'products',
-                'revisions'   => $revisions,
                 'data'        => $data,
-                'categories'  => $categories
             ]);
         }
     }
 
     public function delete($id) {
         if(!empty($_POST)) {
-            $model = new ProductsModel();
+            $model = new PostsModel();
             $file  = $model->find($id)->media;
             unlink(__DIR__ . '/../../public/uploads/' . $file);
             $model->delete($id);
@@ -222,7 +146,7 @@ class PostsController extends Controller {
         }
 
         else {
-            $model = new ProductsModel();
+            $model = new PostsModel();
             $data  = $model->find($id);
             $this->render('pages/admin/products_delete.twig', [
                 'title'       => 'Delete product',
@@ -231,63 +155,6 @@ class PostsController extends Controller {
                 'data'        => $data
             ]);
         }
-    }
-
-    public function search($get) {
-        $query = isset($get['query']) ? $get['query'] : '';
-        $order = isset($get['order']) ? $get['order'] : '';
-
-        $model = new ProductsModel();
-        $data  = $model->search($order, $query);
-
-        echo json_encode($data);
-    }
-
-    public function api($id = null) {
-        if($id) {
-            $model = new ProductsModel();
-            $data  = $model->find($id);
-
-            $data->media = Settings::getConfig()['url'] . 'uploads/' . $data->media;;
-
-            header('Content-Type: application/json');
-            echo json_encode($data);
-        }
-
-        else {
-            $model = new ProductsModel();
-            $data  = $model->all();
-
-            foreach($data as $key => $element) {
-                $data[$key]->media = Settings::getConfig()['url'] . 'uploads/' . $data[$key]->media;
-            }
-
-            header('Content-Type: application/json');
-            echo json_encode($data);
-        }
-    }
-
-    public function stats() {
-        $model = new ProductsModel();
-        $value = $model->value();
-        $count = $model->count();
-        $average_quantity = $model->average('quantity');
-        $average_price    = $model->average('price');
-        $lows_products    = $model->low(5);
-
-        $model2 = new CategoriesModel();
-        $categories_value = $model2->allotment();
-
-        $stats = [
-            'value' => $value,
-            'count' => $count,
-            'average_quantity' => $average_quantity,
-            'average_price'    => $average_price,
-            'categories'       => $categories_value,
-            'lows_products'    => $lows_products
-        ];
-
-        echo json_encode($stats);
     }
 
 }
